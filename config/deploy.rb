d31506dbe74b376e6ffe7e4c9cef3503bb2ca4d7
Lanvige Jiang <lanvige@gmail.com>
2014-02-21 16:34:13 +0800

    add cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,30 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :user, 'deployer'
+set :application, 'simply'
+set :repo_url, 'https://github.com/lanvige/simply.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, "/home/#{fetch :user}/apps/#{fetch :application}"
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
+# Config for rbenv on server.
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.1.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
 
-# Default value for :pty is false
-# set :pty, true
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
@@ -31,8 +35,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
