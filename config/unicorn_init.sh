498d360c8ac108a4307d7ab29523f69d0dbcb359
Lanvige Jiang <lanvige@gmail.com>
2014-03-05 14:35:55 +0800

    try another init script

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -1,84 +1,76 @@
-#!/bin/sh
+#! /bin/sh
+ 
 ### BEGIN INIT INFO
 # Provides:          unicorn
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
+# Required-Start:    $all
+# Required-Stop:     $network $local_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Manage unicorn server
-# Description:       Start, stop, restart unicorn server for a specific application.
+# Short-Description: starts the unicorn web server
+# Description:       starts unicorn
 ### END INIT INFO
-set -e
  
-# Feel free to change any of the following variables for your app:
-TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/ares/apps/simply
-PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=ares
-set -u
+# Change these to match your app:
+USER=ares
+APP_NAME=simply
+APP_PATH=/home/ares/apps/simply
+ENV=development
+RBENV_RUBY_VERSION=2.1.1
  
-OLD_PIN="$PID.oldbin"
+# env
+APP_ROOT="$APP_PATH/$APP_NAME"
+RBENV_ROOT="/home/$USER/.rbenv"
+PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"
+SET_PATH="cd $APP_ROOT; rbenv rehash; rbenv local $RBENV_RUBY_VERSION"
+DAEMON="bundle exec unicorn_rails"
+DAEMON_OPTS="-c $APP_ROOT/config/unicorn.rb -E $ENV -D"
+CMD="$SET_PATH; $DAEMON $DAEMON_OPTS"
+NAME=unicorn
+DESC="Unicorn app for $APP_NAME"
+PID="$APP_ROOT/tmp/pids/unicorn.pid"
+OLD_PID="$PID.oldbin"
+ 
+cd $APP_ROOT || exit 1
  
 sig () {
-  test -s "$PID" && kill -$1 `cat $PID`
+        test -s "$PID" && kill -$1 `cat $PID`
 }
  
 oldsig () {
-  test -s $OLD_PIN && kill -$1 `cat $OLD_PIN`
+        test -s $OLD_PID && kill -$1 `cat $OLD_PID`
 }
  
-run () {
-  if [ "$(id -un)" = "$AS_USER" ]; then
-    eval $1
-  else
-    su -c "$1" - $AS_USER
-  fi
-}
- 
-case "$1" in
+case ${1-help} in
 start)
-  sig 0 && echo >&2 "Already running" && exit 0
-  run "$CMD"
-  ;;
+        sig 0 && echo >&2 "Already running" && exit 0
+        su - $USER -c "$CMD"
+        ;;
 stop)
-  sig QUIT && exit 0
-  echo >&2 "Not running"
-  ;;
+        sig QUIT && exit 0
+        echo >&2 "Not running"
+        ;;
 force-stop)
-  sig TERM && exit 0
-  echo >&2 "Not running"
-  ;;
+        sig TERM && exit 0
+        echo >&2 "Not running"
+        ;;
 restart|reload)
-  sig HUP && echo reloaded OK && exit 0
-  echo >&2 "Couldn't reload, starting '$CMD' instead"
-  run "$CMD"
-  ;;
+        sig HUP && echo reloaded OK && exit 0
+        echo >&2 "Couldn't reload, starting '$CMD' instead"
+        su - $USER -c "$CMD"
+        ;;
 upgrade)
-  if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
-  then
-    n=$TIMEOUT
-    while test -s $OLD_PIN && test $n -ge 0
-    do
-      printf '.' && sleep 1 && n=$(( $n - 1 ))
-    done
-    echo
- 
-    if test $n -lt 0 && test -s $OLD_PIN
-    then
-      echo >&2 "$OLD_PIN still exists after $TIMEOUT seconds"
-      exit 1
-    fi
-    exit 0
-  fi
-  echo >&2 "Couldn't upgrade, starting '$CMD' instead"
-  run "$CMD"
-  ;;
-reopen-logs)
-  sig USR1
-  ;;
+        sig USR2 && exit 0
+        echo >&2 "Couldn't upgrade, starting '$CMD' instead"
+        su - $USER -c "$CMD"
+        ;;
+rotate)
+        sig USR1 && echo rotated logs OK && exit 0
+        echo >&2 "Couldn't rotate logs" && exit 1
+        ;;
 *)
-  echo >&2 "Usage: $0 <start|stop|restart|upgrade|force-stop|reopen-logs>"
-  exit 1
-  ;;
+        echo >&2 "Usage: $0 <start|stop|restart|upgrade|rotate|force-stop>"
+        exit 1
+        ;;
 esac
+ 
+exit 0
